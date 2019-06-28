4cbe28c7cc97f7a11f4448da23496138d980bcc3
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2019-06-28 13:16:15 +0200

    created coming soon page

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,40 +7,33 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import {useSpring, animated} from 'react-spring'
 
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
+
+  const props = useSpring({
+    transform: 'translateY(0)',
+    opacity: 1,
+    from: {
+      transform: 'translateY(100px)',
+      opacity: 0,
     }
-  `)
+  })
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <animated.div style={props}>
+        <div className="maincontent">
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </animated.div>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,26 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <svg width="36" height="44" viewBox="0 0 36 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M20 0H4H0V4V40V44H4H32H36V40V16V12H32V8H28V4H24V0H20ZM28 12V8H24V12H28ZM20 12V4H4V40H32V16H24H20V12Z" fill="#000"/>
+    </svg>
+    <h1 className="headline">Pixel Art Icons</h1>
+    <div>Coming soon...</div>
+    <a
+      className="button"
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://www.instagram.com/pixelarticons"
+    >
+      <span role="img" aria-label="pointer-right">👉 </span> 
+      Get notified on Intagram
+      <span role="img" aria-label="pointer-left"> 👈</span>
+    </a>
   </Layout>
 )
 
