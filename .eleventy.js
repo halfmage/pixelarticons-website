00debf05ad06a3a943cbbe4f0bfb217ff66c7f1c
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2021-03-10 21:24:25 +0100

    migration to 11ty, icons from npm

---
+++ .eleventy.js
@@ -0,0 +1,35 @@
+const htmlmin = require("html-minifier");
+const svgContents = require("eleventy-plugin-svg-contents");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false);
+
+  // Custom files
+  eleventyConfig.addWatchTarget("./_tmp/style.css");
+
+  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
+
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
+    "./node_modules/clipboard/dist/clipboard.min.js": "./js/clipboard.js",
+  });
+
+  eleventyConfig.addPlugin(svgContents);
+
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (
+      process.env.ELEVENTY_PRODUCTION &&
+      outputPath &&
+      outputPath.endsWith(".html")
+    ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
+};
---
+++ styles/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    content: ["_site/**/*.html"],
+    options: {
+      whitelist: [],
+    },
+  },
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
