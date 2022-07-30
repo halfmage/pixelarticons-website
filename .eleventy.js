a1c62f36b97515738297038cc92e511ed9738dea
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2022-07-30 09:21:30 +0200

    more website updates

--- .eleventy.js
+++ .eleventy.js
@@ -13,7 +13,7 @@ module.exports = function (eleventyConfig) {
   })
 
   // Passthroughs
-  eleventyConfig.addPassthroughCopy("social.jpg");
+  eleventyConfig.addPassthroughCopy("social.png");
   eleventyConfig.addPassthroughCopy("./node_modules/pixelarticons/svg");
   eleventyConfig.addPassthroughCopy({"./_tmp/style.css": "./style.css"});
   eleventyConfig.addPassthroughCopy({"./node_modules/clipboard/dist/clipboard.min.js": "./js/clipboard.js"});
